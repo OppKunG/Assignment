0bdc9fc86b5de223bb998a660a81abc65eaf103e
ASUS <ASUS@DESKTOP-TM5K602>
2020-08-23 12:59:05 +0700

    Multiplication Calculation Program Ver.1.0.0

--- Multiplication Program.cpp
+++ Multiplication Program.cpp
@@ -2,17 +2,30 @@
 #include<conio.h>
 int main()
 {
-	int in1,in2,out,ans;
-	printf("--------------------------------------------------------------------------------\n|                  * *     *     *     ***     ***     *                       |\n|                  **      * * * *      *       *      *                       |\n|                  * *     *  *  *     ***      *      ****                    |");
-	printf("\n--------------------------------------------------------------------------------\n********************** Only Computer Engineer to do this ***********************");
-	printf("\n\n\t\t       *Multiplication Calculation Program*\n\t\t\t\t   Ver.Beta");
-	printf("\nInput Number : ");
-	scanf_s("%d %d",&in1,&in2);
-	ans = in1 * in2;
-	printf("\n");
-	for (int i = 1; i <= 12;i++) {
-		out = ans * i;
-		printf("%d * %d = %d\n",ans,i,out);
+	int in1,in2,out,ans,b;
+	char ask[] = "Answer(number)";
+
+	printf("Start Program?\t1.Yes\t2.No\t%s : ", ask);
+	scanf_s("%d", &b);
+	while (b == 1)
+	{
+		printf("\n\n--------------------------------------------------------------------------------\n|                  * *     *     *     ***     ***     *                       |\n|                  **      * * * *      *       *      *                       |\n|                  * *     *  *  *     ***      *      ****                    |");
+		printf("\n--------------------------------------------------------------------------------\n********************** Only Computer Engineer to do this ***********************");
+		printf("\n\n\t\t       *Multiplication Calculation Program*\n\t\t\t\t   Ver.1.0.0");
+		printf("\nInput Number : ");
+		scanf_s("%d %d", &in1, &in2);
+		ans = in1 * in2;
+		printf("\n");
+		for (int i = 1; i <= 12; i++) {
+			out = ans * i;
+			printf("%d * %d = %d\n", ans, i, out);
+		}
+		printf("\nTry again?\t1.Yes\t2.No\t%s : ", ask);
+		scanf_s("%d", &b);
+		if (b == 2)
+		{
+			break;
+		}
 	}
 	return 0;
 }
