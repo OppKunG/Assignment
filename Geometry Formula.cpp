47382d7f85dc56bd88281184820ada660da76b38
ASUS <ASUS@DESKTOP-TM5K602>
2020-08-29 23:27:53 +0700

    Geometry Formula Program v.3.5.0

--- Geometry Formula.cpp
+++ Geometry Formula.cpp
@@ -1,10 +1,12 @@
 #include<stdio.h>
 #include<conio.h>
+#include<math.h>
+
 int main() 
 {
 	int p,d1,d2,c1,c2,c3,c4,b;
-	char ask[]="Answer(number)",sa[]="Surface Area",v[]="Volume";
-	float ans, w, l, r,s,h;
+	char ask[]="Answer(number)",pm[]="Perimeter",a[]="Area",sa[]="Surface Area",v[]="Volume";
+	float ans, w, l, r,s,h,pi=3.14159,s1,s2,s3,S,S1;
 	
 	printf("Start Program?\t1.Yes\t2.No\t%s : ",ask);
 	scanf_s("%d", &b);
@@ -12,15 +14,15 @@ int main()
 	{
 		printf("\n\n--------------------------------------------------------------------------------\n|                  * *     *     *     ***     ***     *                       |\n|                  **      * * * *      *       *      *                       |\n|                  * *     *  *  *     ***      *      ****                    |");
 		printf("\n--------------------------------------------------------------------------------\n********************** Only Computer Engineer to do this ***********************");
-		printf("\n\n\t\t\t    *Geometry Formula Program*\n\t\t\t\t      v.3.0.0");
+		printf("\n\n\t\t\t    *Geometry Formula Program*\n\t\t\t\t      v.3.5.0");
 		printf("\n\n\t\t\t1. 2D Dimension\t\t2. 3D Dimension\n%s : ",ask);
 		scanf_s("%d", &p);
 //2D Dimension
 		if (p == 1)
 		{
-			printf("\n***Choose Your Shape to calculate***\n\n\t\t1.Square\t2.Circle\t3.Rectangle\n%s : ",ask);
+			printf("\n***Choose Your Shape to calculate***\n\n\t\t1.Square\t2.Circle\t3.Rectangle\t4.Triangle\n%s : ",ask);
 			scanf_s("%d", &d1);
-			if (d1 == 1 || d1 == 3)
+			if (d1 == 1 || d1 == 3 || d1 == 4)
 			{
 				printf("\n***What your want to calculate***\n\n\t\t1.Perimeter\t2.Area\n%s : ",ask);
 				scanf_s("%d", &c1);
@@ -32,12 +34,12 @@ int main()
 					if (c1 == 1)
 					{
 						ans = 4 * s;
-						printf("Square Perimeter : %.2f\n", ans);
+						printf("\nSquare %s : %.2f\n",pm, ans);
 					}
 					if (c1 == 2)
 					{
 						ans = s * s;
-						printf("\nSquare Area : %.2f\n", ans);
+						printf("\nSquare %s : %.2f\n",a, ans);
 					}
 				}
 //Rectangle
@@ -45,17 +47,46 @@ int main()
 				{
 					printf("\nWidth : ");
 					scanf_s("%f", &w);
-					printf("Length : ");
+					printf("\nLength : ");
 					scanf_s("%f", &l);
-					if (c1==1)
+					if (c1 == 1)
 					{
 						ans = (w + l) * 2;
-						printf("\nRectangle Perimeter : %.2f\n", ans);
+						printf("\nRectangle %s : %.2f\n",pm, ans);
 					}
-					if (c1==2) 
+					if (c1 == 2) 
 					{
 						ans = w * l;
-						printf("\nRectangle Area : %.2f\n", ans);
+						printf("\nRectangle %s : %.2f\n",a, ans);
+					}
+				}
+//Triangle
+				if (d1 == 4)
+				{
+					printf("\nSide 1 : ");
+					scanf_s("%f", &s1);
+					printf("\nSide 2 : ");
+					scanf_s("%f", &s2);
+					printf("\nSide 3 : ");
+					scanf_s("%f", &s3);
+					if (c1 == 1)
+					{
+						ans = s1+s2+s3;
+						printf("\nTriangle %s : %.2f\n",pm, ans);
+					}
+					if (c1 == 2)
+					{
+						S = (s1 + s2 + s3) / 2;
+						S1 = S*(S-s1)*(S-s2)*(S-s3);
+						ans = sqrt(S1);
+						if (S1 > 0)
+						{
+							printf("\nTriangle %s : %.2f\n", a, ans);							
+						}
+						if (S1 <= 0)
+						{
+							printf("\nThat 3 Side. It can't be triangle.\n");
+						}
 					}
 				}
 			}
@@ -66,15 +97,14 @@ int main()
 				scanf_s("%d", &c2);
 				printf("\nRadius : ");
 				scanf_s("%f", &r);
-
 				if (c2 == 1)
 				{
-					ans = 2 * 3.14 * r;
+					ans = 2 * pi * r;
 					printf("\nCircumference : %.2f\n", ans);
 				}
 				if (c2 == 2)
 				{
-					ans = 3.14 * r * r;
+					ans = pi * r * r;
 					printf("\nCircle Area : %.2f\n", ans);
 				}
 			}
@@ -82,7 +112,7 @@ int main()
 //3D Dimension
 		if (p == 2)
 		{
-			printf("\n***Choose Your Shape to calculate***\n\n\t\t1.Cube\t2.Cylinder\t3.Cone\t4.Sphere\n%s : ",ask);
+			printf("\n***Choose Your Shape to calculate***\n\n\t1.Cube\t2.Cylinder\t3.Cone\t4.Sphere\t5.Rectangular Prism\n%s : ",ask);
 			scanf_s("%d", &d2);
 			printf("\n***What your want to calculate***\n\n\t\t1.Surface Area\t2.Volume\n%s : ",ask);
 			scanf_s("%d", &c4);
@@ -91,14 +121,15 @@ int main()
 			{
 				printf("\nSide : ");
 				scanf_s("%f", &s);
-				if(c4==1)
+				if(c4 == 1)
 				{
 					ans = 6*s*s;
-					printf("Cube %s : %.2f\n",sa, ans);
+					printf("\nCube %s : %.2f\n",sa, ans);
 				}
-				if(c4==2){
+				if(c4 == 2)
+				{
 					ans = s*s*s;
-					printf("Cube %s : %.2f\n",v, ans);
+					printf("\nCube %s : %.2f\n",v, ans);
 				}
 				
 			}
@@ -107,17 +138,17 @@ int main()
 			{
 				printf("\nRadius : ");
 				scanf_s("%f", &r);
-				printf("Height : ");
+				printf("\nHeight : ");
 				scanf_s("%f", &h);
-				if(c4==1)
+				if(c4 == 1)
 				{
-					ans = (2*3.14*r*h)+(2*3.14*r*r);
-					printf("Cylinder %s : %.2f\n",v, ans);
+					ans = (2*pi*r*h)+(2*pi*r*r);
+					printf("\nCylinder %s : %.2f\n",sa, ans);
 				}
-				if(c4==2)
+				if(c4 == 2)
 				{
-					ans = 3.14*r*r*h;
-					printf("Cylinder %s : %.2f\n",v, ans);
+					ans = pi*r*r*h;
+					printf("\nCylinder %s : %.2f\n",v, ans);
 				}
 			}
 //Cone
@@ -129,14 +160,14 @@ int main()
 				scanf_s("%f", &l);
 				printf("\nHeight : ");
 				scanf_s("%f", &h);
-				if(c4==1)
+				if(c4 == 1)
 				{
-					ans = 3.14*r*l;
+					ans = pi*r*l;
 					printf("\nCone %s : %.2f\n",sa, ans);
 				}
-				if(c4==2)
+				if(c4 == 2)
 				{
-					ans = (3.14*r*r*h)/3;
+					ans = (pi*r*r*h)/3;
 					printf("\nCone %s : %.2f\n",v, ans);
 				}
 			}
@@ -145,15 +176,35 @@ int main()
 			{
 				printf("\nRadius : ");
 				scanf_s("%f", &r);
-				if(c4==1)
+				if(c4 == 1)
 				{
-					ans = 4*3.14*r*r;
+					ans = 4*pi*r*r;
 					printf("\nSphere %s : %.2f\n",sa, ans);
 				}
-				if(c4==2)
+				if(c4 == 2)
 				{
-						ans = (4*3.14*r*r*r)/3;
+						ans = (4*pi*r*r*r)/3;
 					printf("\nSphere %s : %.2f\n",v, ans);
+				}
+			}
+//Rectangular Prism
+			if (d2 == 5)
+			{
+				printf("\nWidth : ");
+				scanf_s("%f", &w);
+				printf("\nLength : ");
+				scanf_s("%f", &l);
+				printf("\nHeight : ");
+				scanf_s("%f", &h);
+				if (c4 == 1)
+				{
+					ans = 2 * ((l * w) + (l * h) + (w * h));
+					printf("\nRectangular Prism %s : %.2f\n", sa, ans);
+				}
+				if (c4 == 2)
+				{
+					ans = w * l * h;
+					printf("\nRectangular Prism %s : %.2f\n", v, ans);
 				}
 			}
 		}
