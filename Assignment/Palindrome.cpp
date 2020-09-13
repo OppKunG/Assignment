cebc45071064acc0566c4b76bb40e0768103710d
ASUS <ASUS@DESKTOP-TM5K602>
2020-09-13 10:48:38 +0700

    Pointer Activity

--- Assignment/Palindrome.cpp
+++ Assignment/Palindrome.cpp
@@ -15,45 +15,36 @@ int count_character(char str[50])
 
 int main()
 {
-	char str[50],*p,*l, ask[] = "Answer(number)";
-	int show=0,b,size;
-	printf("Start Program?\t1.Yes\t2.No\t%s : ", ask);
-	scanf_s("%d", &b);
-	while (b == 1)
+	char str[50],*p,*l;
+	int show=0,no=0,size;
+
+	printf("\n\n--------------------------------------------------------------------------------\n|                  * *     *     *     ***     ***     *                       |\n|                  **      * * * *      *       *      *                       |\n|                  * *     *  *  *     ***      *      ****                    |");
+	printf("\n--------------------------------------------------------------------------------\n********************** Only Computer Engineer to do this ***********************");
+	printf("\n\n\t\t\t    *Palindrome Program*\n\n");
+	printf("Input : ");
+	scanf_s("%s",&str,49);
+	size = count_character(str);
+	p = &str[0];
+	l = &str[size-1];
+	while (l > p)
 	{
-		printf("\n\n--------------------------------------------------------------------------------\n|                  * *     *     *     ***     ***     *                       |\n|                  **      * * * *      *       *      *                       |\n|                  * *     *  *  *     ***      *      ****                    |");
-		printf("\n--------------------------------------------------------------------------------\n********************** Only Computer Engineer to do this ***********************");
-		printf("\n\n\t\t\t    *Palindrome Program*\n\n");
-		printf("Input : ");
-		scanf_s("%s",&str,49);
-		size = count_character(str);
-		p = &str[0];
-		l = &str[size-1];
-		while (l > p)
+		if (*l != *p)
 		{
-			if (*l != *p)
-			{
-				show = 1;
-				break;
-			}
-			p++;
-			l--;
-		}
-		if (show == 1)
-		{
-			printf("\nNot Palindrome\n");
-		}
-		else
-		{
-			printf("\nPalindrome\n");
-		}
-		printf("\n%d Characters\n", size);
-		printf("\nTry again?\t1.Yes\t2.No\t%s : ", ask);
-		scanf_s("%d", &b);
-		if (b == 2)
-		{
+			show = 1;
+			no = 1;
 			break;
 		}
+		p++;
+		l--;
 	}
+	if (show == 1||no == 1)
+	{
+		printf("\nNot Palindrome\n");
+	}
+	else
+	{
+		printf("\nPalindrome\n");
+	}
+	printf("\n%d Characters\n", size);
 	return 0;
 }
